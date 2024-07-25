bed35f97832ec490bb77ed011f80bee9708305c9
Palki <132294980+PalkiDB1@users.noreply.github.com>
2024-07-26 01:24:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed= new Typed(".typing",{
+    strings:["Professional Developer", "Singer", "Event coordinator head of Innovation club","design team member of Codechef","Good team player" ],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true,
+})
